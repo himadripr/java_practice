cc84c1d72b4011fdee6fd1ef82a58aa58252f42a
RT <himadri.p@tbitsglobal.com>
2019-05-02 20:55:17 +0530

    Even Odd printing using two threads.

---
+++ java_practice/src/main/java/org/himadri/practice/java_practice/java_examples/PrintEvenOddTester.java
@@ -0,0 +1,87 @@
+package org.himadri.practice.java_practice.java_examples;
+public class PrintEvenOddTester {
+
+    public static void main(String... args) {
+        Printer print = new Printer();
+        Thread t1 = new Thread(new TaskEvenOdd(print, 10, false));
+        Thread t2 = new Thread(new TaskEvenOdd(print, 10, true));
+        
+        t1.start();
+        t2.start();
+    }
+
+}
+
+class TaskEvenOdd implements Runnable {
+
+    private int max;
+    private Printer print;
+    private boolean isEvenNumber;
+
+    TaskEvenOdd(Printer print, int max, boolean isEvenNumber) {
+        this.print = print;
+        this.max = max;
+        this.isEvenNumber = isEvenNumber;
+    }
+
+    @Override
+    public void run() {
+
+        //System.out.println("Run method");
+        int number = isEvenNumber == true ? 2 : 1;
+        while (number <= max) {
+
+            if (isEvenNumber) {
+                //System.out.println("Even :"+ Thread.currentThread().getName());
+                print.printEven(number);
+                //number+=2;
+            } else {
+                //System.out.println("Odd :"+ Thread.currentThread().getName());
+                print.printOdd(number);
+                // number+=2;
+            }
+            number += 2;
+            try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        }
+
+    }
+
+}
+
+class Printer {
+
+    boolean isEven = true;
+
+    synchronized void printEven(int number) {
+
+        while (isEven == false) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Even:" + number);
+        isEven = false;
+        notifyAll();
+    }
+
+    synchronized void printOdd(int number) {
+        while (isEven == true) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Odd:" + number);
+        isEven = true;
+        notifyAll();
+    }
+
+}
